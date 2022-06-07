215f9cfe7b18f281d00e34a129876c8f09c161f5
Barbara CANO <bcano@e1r13p9.42.fr>
2022-06-07 18:37:47 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcano <bcano@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/07 12:19:46 by bcano             #+#    #+#             */
+/*   Updated: 2022/06/07 12:45:17 by bcano            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <ctype.h>
+
+int    display_error(int type, const char *err, const char *message)
+{
+    std::cout << "./ircserv: error: " << err << std::endl;
+    std::cout << message << std::endl;
+    return (type);
+}
+
+int main(int argc, char **argv)
+{
+    int port = 0;
+    std::string pwd("");
+    
+    if (argc != 3)
+        return (display_error(1, "Wrong numbers of Parameters","Usage: ./ircserv <port> <password>"));
+    for (int i ( 0 ); argv[1][i]; i++)
+        if (!(isdigit(argv[1][i])))
+            return (display_error(1, "Port must be a number", "Usage: ./ircserv <port> <password>"));
+    if (atoi(argv[1]) <= 0)
+        return (display_error(1, "Port must be strictly number", "Usage: ./ircserv <port> <password>"));
+    port = atoi(argv[1]);
+    pwd = argv[2];
+    std::cout << port << " " << pwd << std::endl;
+    return (0);
+}
