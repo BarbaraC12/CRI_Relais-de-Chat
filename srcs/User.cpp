5b386c229cdf2718cd4594a77fdfb0a1cccedb49
Barbara CANO <bcano@e1r3p4.42.fr>
2022-06-24 21:10:08 +0200

    ppp

--- srcs/User.cpp
+++ srcs/User.cpp
@@ -6,7 +6,7 @@
 /*   By: bcano <bcano@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 12:58:28 by bcano             #+#    #+#             */
-/*   Updated: 2022/06/24 20:57:05 by bcano            ###   ########.fr       */
+/*   Updated: 2022/06/24 21:09:36 by bcano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,11 @@ User::User(User const &src)
 	return;
 }
 
-User::~User(void)
-{
+User::~User(void) {
 	return;
 }
 
-User &User::operator=(User const &rhs)
-{
+User &User::operator=(User const &rhs) {
 	if (this != &rhs)
 	{
 		this->nickname = rhs.nickname;
@@ -49,94 +47,70 @@ User &User::operator=(User const &rhs)
 }
 
 void		User::setNickname( std::string nickN) {
-
 	this->_nickName = nickN;	
 }
 
 void		User::setUsername( std::string userN ) {
-
 	this->_userName = userN;
-
 }
 
 void		User::setHostname( std::string hostN ) {
-
 	this->_hostName = hostN;
-	
-}
+	}
 
 void		User::setServname( std::string serverN ) {
-
 	this->_servName = serverN;
-	
-}
+	}
 
 void		User::setRealname( std::string realN ) {
-
 	this->_realName = realN;
-	
-}
+	}
 
 void		User::setUsermode( std::string userM ) {
-
 	this->_userMode = userM;
-	
-}
+	}
 
 void		User::addChanel( std::string ) {
-
-	
-}
+	}
 
 void		User::setStatus( bool status ) {
-
 	this->_online = status;	
-
 }
 
 int				User::getSd( void ) {
-
 	return this->_sd;
 }
 
 std::string		User::getNickname( void ) {
-
 	return this->_nickName;
 }
 
 std::string		User::getUsername( void ) {
-
 	return this->_userName;
 }
 
 std::string		User::getHostname( void ) {
-
 	return this->_hostName;
 }
 
 std::string		User::getServname( void ) {
-
 	return this->_servName;
 }
 
 std::string		User::getRealname( void ) {
-
 	return this->_realName;
 }
 
 std::string		User::getUsermode( void ) {
-
 	return this->_userName;
 }
 
-std::string		User::getChanels( void ) {
-	
+std::string		User::getChanels( void ) {	
 	std::string st("this chan");
 	(void)this->_chan;
 	return st;
 }
 
 bool			User::getStatus( void ) {
-
 	return this->_online;
 }
