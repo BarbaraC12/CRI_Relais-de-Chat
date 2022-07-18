0729094d35500a05d7a600c1b5e399b1c59c74c5
Barbara CANO <bcano@e2r1p6.clusters.42paris.fr>
2022-07-18 16:24:37 +0200

    oups i already did a mistake ^^'

--- srcs/User.cpp
+++ srcs/User.cpp
@@ -42,104 +42,134 @@ User &User::operator=(User const &rhs) {
 *                    SETTERS                    *
 * ********************************************* */
 
-void 		User::setSd(int fd) {
+void 	User::setSd(int fd) {
+
 	this->_sd = fd;
 }
 
-void		User::setStatus( e_user_status stat ) {
+void	User::setStatus( e_user_status stat ) {
+
 	this->_status = stat;	
 }
 
-void		User::addNickname( std::string nickN) {
+void	User::addNickname( std::string nickN) {
+
 	this->_nickName.push_back(nickN);	
 }
 
-void		User::setUsername( std::string userN ) {
+void	User::setUsername( std::string userN ) {
+
 	this->_userName = userN;
 }
 
-void		User::setHostname( std::string hostN ) {
+void	User::setHostname( std::string hostN ) {
+
 	this->_hostName = hostN;
 }
 
-void		User::setServname( std::string serverN ) {
+void	User::setServname( std::string serverN ) {
+
 	this->_servName = serverN;
 }
 
-void		User::setRealname( std::string realN ) {
+void	User::setRealname( std::string realN ) {
+
 	this->_realName = realN;
 }
 
-void		User::setUsermode( e_user_mode userM, bool status ) {
+void	User::setUsermode(
+	e_user_mode userM, bool status ) {
+
 	this->_userMode[userM] = status;
 }
 
-void		User::setConnectTime( std::time_t ) {
+void	User::setConnectTime( std::time_t ) {
+
 	this->_connectTime = time(NULL);
 }
 
-void		User::setLastPong( std::time_t ) {
+void	User::setLastPong( std::time_t ) {
+
 	this->_lastPong = time(NULL);
 }
 
-void		User::addChanel( std::string ) {
+void	User::addChanel( std::string ) {
+
 }
 
-/* ******************************************** *
-*                    GETTERS                    *
-* ********************************************* */
-
-int			User::getSd( void ) {
-	return this->_sd;
-}
-
-e_user_status	User::getStatus( void ) {
-	return this->_status;
-}
-
-std::string		User::getNickname( void ) {
-	return this->_nickName.back();
-}
-
-std::vector<std::string>	User::getNickname_history( void ) {
-	return this->_nickName;
-}
-
-std::string		User::getUsername( void ) {
-	return this->_userName;
-}
-
-std::string		User::getHostname( void ) {
-	return this->_hostName;
-}
-
-std::string		User::getServname( void ) {
-	return this->_servName;
-}
-
-std::string		User::getRealname( void ) {
-	return this->_realName;
-}
-
-bool		User::getUsermode( void ) {
-	return this->_userMode[O];
-}
-
-std::string		User::getChanels( void ) {	
-	std::string st("this chan");
-	(void)this->_chan;
-	return st;
-}
-
-std::time_t		getConnectTime( void );
-
-std::time_t		getLastPong( void );
-
-void  User::set_user_params(std::string uname, std::string host, std::string server, std::string realname) {
+void	User::set_user_params(
+	std::string uname, std::string host,
+	std::string server, std::string realname ) {
 
 	this->_userName = uname;
 	this->_hostName = host;
 	this->_servName = server;
 	this->_realName = realname;
 	this->_status = REGISTER;
+}
+
+/* ******************************************** *
+*                    GETTERS                    *
+* ********************************************* */
+
+int							User::getSd( void ) {
+
+	return this->_sd;
+}
+
+e_user_status				User::getStatus( void ) {
+
+	return this->_status;
+}
+
+std::string					User::getNickname( void ) {
+
+	return this->_nickName.back();
+}
+
+std::vector<std::string>	User::getNickname_history( void ) {
+
+	return this->_nickName;
+}
+
+std::string					User::getUsername( void ) {
+
+	return this->_userName;
+}
+
+std::string					User::getHostname( void ) {
+
+	return this->_hostName;
+}
+
+std::string					User::getServname( void ) {
+
+	return this->_servName;
+}
+
+std::string					User::getRealname( void ) {
+
+	return this->_realName;
+}
+
+bool						User::getUsermode( void ) {
+
+	return this->_userMode[O];
+}
+
+std::string					User::getChanels( void ) {
+
+	std::string st("this chan");
+	(void)this->_chan;
+	return st;
+}
+
+std::time_t					User::getConnectTime( void ) {
+
+	return this->_connectTime;
+}
+
+std::time_t					User::getLastPong( void ) {
+
+	return this->_lastPong;
 }
